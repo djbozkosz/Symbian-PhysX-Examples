725f5801880c9b42b7ef30bd758e71f78eebb91a
Tomáš Růžička <t_ruzicka@email.cz>
2018-07-09 23:53:12 +0200

    * Updated main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <QApplication>
-#include <QSplashScreen>
+#include <QCoreApplication>
 #include <QPixmap>
 
+#include "Splash.h"
 #include "GlWidget.h"
 #include "Sample00.h"
 
@@ -9,22 +10,23 @@ int main(int argc, char *argv[])
 {
 	QApplication application(argc, argv);
 
-	QSplashScreen splash(QPixmap(":/images/logo.png"));
-	splash.show();
-	splash.setStyleSheet("background-color: black;");
+	QCoreApplication::setAttribute(Qt::AA_S60DisablePartialScreenInputMode, false);
+
+	Splash splash;
+	splash.showFullScreen();
+
 	application.processEvents();
 
 	Sample00 sample00;
-	GlWidget glWidget;
+	GlWidget glWidget(NULL, &splash, 2000);
 
 	QObject::connect(&sample00, SIGNAL(Simulated()), &glWidget, SLOT(update()));
-	QObject::connect(&sample00, SIGNAL(ActorBoxAdded(const ISceneObjectProvider*, QVector3D)), &glWidget, SLOT(AddBox(const ISceneObjectProvider*, QVector3D)));
-	QObject::connect(&sample00, SIGNAL(ActorMeshAdded(const ISceneObjectProvider*, QVector3D)), &glWidget, SLOT(AddMesh(const ISceneObjectProvider*, QVector3D)));
+	QObject::connect(&sample00, SIGNAL(ActorBoxAdded(const ISceneObjectProvider*, QVector3D, QVector2D)), &glWidget, SLOT(AddBox(const ISceneObjectProvider*, QVector3D, QVector2D)));
+	QObject::connect(&sample00, SIGNAL(ActorMeshAdded(const ISceneObjectProvider*, QVector3D, QVector2D)), &glWidget, SLOT(AddMesh(const ISceneObjectProvider*, QVector3D, QVector2D)));
 
 	QObject::connect(&glWidget, SIGNAL(Initialized()), &sample00, SLOT(Initialize()));
 
-	glWidget.showMaximized();
-	splash.finish(&glWidget);
+	// delayed glWidget show in Initialize()
 
 	return application.exec();
 }
